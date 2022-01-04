8b26b817e1576056d17fd81fb482a1973362125b
Kai Yao <kai.yao@intel.com>
2022-01-04 11:31:50 +0800

    change to submodule

--- set_env.sh
+++ set_env.sh
@@ -4,8 +4,11 @@ set -x
 conda install -y mkl mkl-include
 conda install -y jemalloc
 
+# init submodules
+git submodule sync
+git submodule update --init --recursive
+
 # install gen-efficientnet-pytorch (submodule)
-git submodule sync && git submodule update --init --recursive
 cd gen-efficientnet-pytorch
 git apply ../gen.patch
 cp ../main.py ./
@@ -13,12 +16,10 @@ cp ../pretrain_setup.py ./
 cp -r ../pretrainedmodels ./
 pip install -r ../requirements.txt
 python pretrain_setup.py install
+cd ..
 
 # install torchvision v0.11.1
 pip uninstall -y torchvision
-git clone https://github.com/pytorch/vision.git
 cd vision
-git checkout v0.11.1
 python setup.py install
 cd ..
-#pip install --no-deps torchvision -f https://download.pytorch.org/whl/torch_stable.html
