62e20ead8c9d3e00a193b70c681e43815cc87830
kaiyaointel <kai.yao@intel.com>
2022-03-11 17:51:25 +0800

    Update and rename run_auto_ipex.sh to run_auto_ipex_broad.sh

--- run_auto_ipex_broad.sh
+++ run_auto_ipex_broad.sh
@@ -2,6 +2,7 @@ set -x
 cd gen-efficientnet-pytorch
 
 model_all="alexnet,densenet121,densenet161,densenet169,densenet201,efficientnet_b0,efficientnet_b1,efficientnet_b2,efficientnet_b3,efficientnet_b4,efficientnet_b5,efficientnet_b6,efficientnet_b7,efficientnet_b8,fbnetc_100,googlenet,inception_v3,mnasnet0_5,mnasnet1_0,resnet101,resnet152,resnet18,resnet34,resnet50,resnext101_32x8d,resnext50_32x4d,shufflenet_v2_x0_5,shufflenet_v2_x1_0,spnasnet_100,squeezenet1_0,squeezenet1_1,vgg11,vgg11_bn,vgg13,vgg13_bn,vgg16,vgg16_bn,vgg19,vgg19_bn,wide_resnet101_2,wide_resnet50_2"
+batch_size=80
 
 MODEL_NAME_LIST=($(echo "${model_all}" |sed 's/,/ /g'))
 
@@ -20,28 +21,37 @@ mkdir logs
 # IPEX FP32
 for model in ${MODEL_NAME_LIST[@]}
 do
-    numactl --cpunodebind=0 --membind=0 python main.py -e --performance --pretrained --dummy --no-cuda -j 1 -w 20 -i 100 -a $model -b 64 --precision "float32" --ipex --jit 2>&1 | tee ./logs/$model-IPEX-FP32.log
+    numactl --cpunodebind=0 --membind=0 python main.py -e --performance --pretrained --dummy --no-cuda -j 1 -w 20 -i 100 -a $model -b ${batch_size} --precision "float32" --ipex --jit 2>&1 | tee ./logs/$model-IPEX-FP32.log
     latency=$(grep "inference latency:" ./logs/$model-IPEX-FP32.log | sed -e 's/.*latency//;s/[^0-9.]//g')
     throughput=$(grep "inference Throughput:" ./logs/$model-IPEX-FP32.log | sed -e 's/.*Throughput//;s/[^0-9.]//g')
     echo $model IPEX FP32 $latency $throughput | tee -a ./logs/summary.log
 done
 
+# IPEX FP32 jit_optimize
+for model in ${MODEL_NAME_LIST[@]}
+do
+    numactl --cpunodebind=0 --membind=0 python main.py -e --performance --pretrained --dummy --no-cuda -j 1 -w 20 -i 100 -a $model -b ${batch_size} --precision "float32" --ipex --jit  --jit_optimize 2>&1 | tee ./logs/$model-IPEX-FP32-opt.log
+    latency=$(grep "inference latency:" ./logs/$model-IPEX-FP32-opt.log | sed -e 's/.*latency//;s/[^0-9.]//g')
+    throughput=$(grep "inference Throughput:" ./logs/$model-IPEX-FP32-opt.log | sed -e 's/.*Throughput//;s/[^0-9.]//g')
+    echo $model IPEX FP32_OPT $throughput | tee -a ./logs/summary.log
+done
+
 # IPEX BF16
 for model in ${MODEL_NAME_LIST[@]}
 do
-    numactl --cpunodebind=0 --membind=0 python main.py -e --performance --pretrained --dummy --no-cuda -j 1 -w 20 -i 100 -a $model -b 64 --precision "bfloat16" --ipex --jit 2>&1 | tee ./logs/$model-IPEX-BF16.log
+    numactl --cpunodebind=0 --membind=0 python main.py -e --performance --pretrained --dummy --no-cuda -j 1 -w 20 -i 100 -a $model -b ${batch_size} --precision "bfloat16" --ipex --jit 2>&1 | tee ./logs/$model-IPEX-BF16.log
     latency=$(grep "inference latency:" ./logs/$model-IPEX-BF16.log | sed -e 's/.*latency//;s/[^0-9.]//g')
     throughput=$(grep "inference Throughput:" ./logs/$model-IPEX-BF16.log | sed -e 's/.*Throughput//;s/[^0-9.]//g')
-    echo $model IPEX BF16 $latency $throughput | tee -a ./logs/summary.log
+    echo $model IPEX BF16 $throughput | tee -a ./logs/summary.log
 done
 
 # IPEX INT8
 for model in ${MODEL_NAME_LIST[@]}
 do
-    numactl --cpunodebind=0 --membind=0 python main.py -e --performance --pretrained --dummy --no-cuda -j 1 -w 20 -i 200 -a $model -b 80 --precision "int8_ipex" --ipex --profile 2>&1 | tee ./logs/$model-IPEX-INT8.log
+    numactl --cpunodebind=0 --membind=0 python main.py -e --performance --pretrained --dummy --no-cuda -j 1 -w 20 -i 100 -a $model -b ${batch_size} --precision "int8_ipex" --ipex 2>&1 | tee ./logs/$model-IPEX-INT8.log
     latency=$(grep "inference latency:" ./logs/$model-IPEX-INT8.log | sed -e 's/.*latency//;s/[^0-9.]//g')
     throughput=$(grep "inference Throughput:" ./logs/$model-IPEX-INT8.log | sed -e 's/.*Throughput//;s/[^0-9.]//g')
-    echo $model IPEX INT8 $latency $throughput | tee -a ./logs/summary.log
+    echo $model IPEX INT8 $throughput | tee -a ./logs/summary.log
 done
 
 # # noIPEX FP32
@@ -50,7 +60,7 @@ done
     # numactl --cpunodebind=0 --membind=0 python main.py -e --performance --pretrained --dummy --no-cuda -j 1 -w 20 -i 100 -a $model -b 64 --precision "float32" --jit 2>&1 | tee ./logs/$model-noIPEX-FP32.log
     # latency=$(grep "inference latency:" ./logs/$model-noIPEX-FP32.log | sed -e 's/.*latency//;s/[^0-9.]//g')
     # throughput=$(grep "inference Throughput:" ./logs/$model-noIPEX-FP32.log | sed -e 's/.*Throughput//;s/[^0-9.]//g')
-    # echo $model noIPEX FP32 $latency $throughput | tee -a ./logs/summary.log
+    # echo $model noIPEX FP32 $throughput | tee -a ./logs/summary.log
 # done
 
 cat ./logs/summary.log
