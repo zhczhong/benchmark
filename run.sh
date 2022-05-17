4cd58fbb6ec2290034adbae41a3c26306bf4a9b8
Zhang, Weizhuo <weizhuo.zhang@intel.com>
2022-05-17 09:23:12 +0000

    debug

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 set -x
 bash run_auto_pt_channellast.sh all float32 56
+cat ./logs/summary.log
 bash run_auto_pt_channellast.sh all bfloat16_brutal 56
+cat ./logs/summary.log
 bash run_auto_pt_channellast.sh all float32 " --fx " 56
+cat ./logs/summary.log
 bash run_auto_pt_channellast.sh all bfloat16_brutal " --fx " 56
+cat ./logs/summary.log
 bash run_auto_pt_channellast.sh all bfloat16 56
+cat ./logs/summary.log
