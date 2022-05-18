b1d945044b74ece805bcdd755b5f607e7113c114
Kai Yao <kai.yao@intel.com>
2022-05-18 17:26:00 +0800

    del print

--- main.py
+++ main.py
@@ -667,7 +667,6 @@ def validate(val_loader, model, criterion, args):
                 # compute output
 
                 if args.torchdynamo_ipex:
-                    print("Running TorchDynamo with IPEX backend")
                     import torchdynamo
                     from torchdynamo.optimizations import backends
                     if args.precision == "float32":
@@ -685,7 +684,6 @@ def validate(val_loader, model, criterion, args):
                             else:
                                 output = model(images)
                 elif args.torchdynamo_fx:
-                    print("Running TorchDynamo with FX (torch.fx.GraphModule) backend")
                     from typing import List
                     import torchdynamo
                     def fx_compiler(gm: torch.fx.GraphModule, example_inputs: List[torch.Tensor]):
