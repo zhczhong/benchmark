644832d2f95cbc241ab6e81f36dc42fb30b87911
kaiyaointel <kai.yao@intel.com>
2022-05-24 09:58:21 +0800

    Create set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -5,6 +5,6 @@ conda install -y mkl mkl-include
 conda install -y jemalloc
 
 # installations
-pip install -r requirements.txt
+# pip install -r requirements.txt
 python pretrain_setup.py install
 rm -rf build dist pretrainedmodels.egg-info
