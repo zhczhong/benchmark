af7e669c91f1f5f15217f35f2420702058f1bde7
kaiyaointel <kai.yao@intel.com>
2022-06-12 05:34:43 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,19 @@
 set -x
 
-bash run_auto_cpu.sh all float32 throughput ipex
-# bash run_auto_cpu.sh all float32 throughput torchdynamo_ipex
-bash run_auto_cpu.sh all float32 throughput pt
-bash run_auto_cpu.sh all float32 throughput ofi
-bash run_auto_cpu.sh all bfloat16 throughput ipex
-# bash run_auto_cpu.sh all bfloat16 throughput torchdynamo_ipex
-bash run_auto_cpu.sh all bfloat16 throughput pt
-bash run_auto_cpu.sh all bfloat16 throughput ofi
-bash run_auto_cpu.sh all int8_ipex throughput int8_ipex
+mode_all=$1
+if [ ${model_all} == "all" ]; then
+    model_all="throughput,latency,multi_instance"
+fi
+mode_list=($(echo "${mode_all}" |sed 's/,/ /g'))
+
+for mode in ${mode_list[@]}
+do
+    bash run_auto_cpu.sh all float32 ${mode} ipex
+    # bash run_auto_cpu.sh all float32 ${mode} torchdynamo_ipex
+    bash run_auto_cpu.sh all float32 ${mode} pt
+    bash run_auto_cpu.sh all float32 ${mode} ofi
+    bash run_auto_cpu.sh all bfloat16 ${mode} ipex
+    # bash run_auto_cpu.sh all bfloat16 ${mode} torchdynamo_ipex
+    bash run_auto_cpu.sh all bfloat16 ${mode} pt
+    bash run_auto_cpu.sh all int8_ipex ${mode} int8_ipex
+done
