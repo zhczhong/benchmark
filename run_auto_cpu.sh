c69acc4926a125864c35b96b584ae3898f9bec50
kaiyaointel <kai.yao@intel.com>
2022-06-12 05:52:36 +0800

    Update run_auto_cpu.sh

--- run_auto_cpu.sh
+++ run_auto_cpu.sh
@@ -12,13 +12,43 @@ if [ ${model_all} == "all" ]; then
     model_all="\
         alexnet,\
         densenet121,densenet161,densenet169,densenet201,\
-        efficientnet_b0,efficientnet_b1,efficientnet_b2,efficientnet_b3,efficientnet_b4,efficientnet_b5,efficientnet_b6,efficientnet_b7,efficientnet_b8,\
-        fbnetc_100,googlenet,inception_v3,\
-        mnasnet0_5,mnasnet1_0,\
-        resnet101,resnet152,resnet18,resnet34,resnet50,resnext101_32x8d,resnext50_32x4d,\
-        shufflenet_v2_x0_5,shufflenet_v2_x1_0,spnasnet_100,squeezenet1_0,squeezenet1_1,\
+        efficientnet_b0,efficientnet_b1,efficientnet_b2,efficientnet_b3,efficientnet_b4,efficientnet_b5,efficientnet_b6,efficientnet_b7,\
+        fbnetc_100,\
+        googlenet,\
+        inception_v3,\
+        mnasnet0_5,mnasnet0_75,mnasnet1_0,mnasnet1_3,\
+        mobilenet_v2,mobilenet_v3_small,mobilenet_v3_large,\
+        resnet18,resnet34,resnet50,resnet101,resnet152,\
+        resnext50_32x4d,resnext101_32x8d,\
+        shufflenet_v2_x0_5,shufflenet_v2_x1_0,shufflenet_v2_x1_5,shufflenet_v2_x2_0,\
+        spnasnet_100,\
+        squeezenet1_0,squeezenet1_1,\
         vgg11,vgg11_bn,vgg13,vgg13_bn,vgg16,vgg16_bn,vgg19,vgg19_bn,\
-        wide_resnet101_2,wide_resnet50_2"
+        wide_resnet50_2,wide_resnet101_2,\
+        vit_b_16,vit_b_32,vit_l_16,vit_l_32,\
+        convnext_tiny,convnext_small,convnext_base,convnext_large,\
+        regnet_y_400mf,regnet_y_800mf,regnet_y_1_6gf,regnet_y_3_2gf,regnet_y_8gf,regnet_y_16gf,regnet_y_32gf,regnet_y_128gf,regnet_x_400mf,regnet_x_800mf,regnet_x_1_6gf,regnet_x_3_2gf,regnet_x_8gf,regnet_x_16gf,regnet_x_32gf"
+fi
+if [ ${model_all} == "key" ]; then
+    model_all="\
+        alexnet,\
+        densenet161,\
+        efficientnet_b3,\
+        fbnetc_100,\
+        googlenet,\
+        inception_v3,\
+        mnasnet1_0,\
+        mobilenet_v3_small,\
+        resnet50,\
+        resnext101_32x8d,\
+        shufflenet_v2_x1_0,\
+        spnasnet_100,\
+        squeezenet1_0,\
+        vgg16,\
+        wide_resnet50_2,\
+        vit_l_16,\
+        convnext_base,\
+        regnet_y_1_6gf"
 fi
 
 model_list=($(echo "${model_all}" |sed 's/,/ /g'))
