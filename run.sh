5c792a2bb73123d877d844512c42a1ede6c206ed
kaiyaointel <kai.yao@intel.com>
2022-06-11 23:59:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 set -x
 
-bash run_auto_pt_channellast.sh all float32 56
-cat ./logs/summary.log
-bash run_auto_pt_channellast.sh all bfloat16_brutal 56
-cat ./logs/summary.log
-bash run_auto_pt_channellast.sh all float32 " --fx " 56
-cat ./logs/summary.log
-bash run_auto_pt_channellast.sh all bfloat16_brutal " --fx " 56
-cat ./logs/summary.log
-bash run_auto_pt_channellast.sh all bfloat16 56
-cat ./logs/summary.log
+bash run_auto_cpu.sh all float32 throughput ipex
+# bash run_auto_cpu.sh all float32 throughput torchdynamo_ipex
+bash run_auto_cpu.sh all float32 throughput pt
+bash run_auto_cpu.sh all float32 throughput ofi
+bash run_auto_cpu.sh all bfloat16 throughput ipex
+# bash run_auto_cpu.sh all bfloat16 throughput torchdynamo_ipex
+bash run_auto_cpu.sh all bfloat16 throughput pt
+bash run_auto_cpu.sh all bfloat16 throughput ofi
+bash run_auto_cpu.sh all int8_ipex throughput int8_ipex
