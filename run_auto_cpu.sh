50a043391d537030b408f98f526e70b8c6f0be18
kaiyaointel <kai.yao@intel.com>
2022-06-12 17:30:00 +0800

    Update run_auto_cpu.sh

--- run_auto_cpu.sh
+++ run_auto_cpu.sh
@@ -27,7 +27,7 @@ if [ ${model_all} == "all" ]; then
         wide_resnet50_2,wide_resnet101_2,\
         vit_b_16,vit_b_32,vit_l_16,vit_l_32,\
         convnext_tiny,convnext_small,convnext_base,convnext_large,\
-        regnet_y_400mf,regnet_y_800mf,regnet_y_1_6gf,regnet_y_3_2gf,regnet_y_8gf,regnet_y_16gf,regnet_y_32gf,regnet_y_128gf,regnet_x_400mf,regnet_x_800mf,regnet_x_1_6gf,regnet_x_3_2gf,regnet_x_8gf,regnet_x_16gf,regnet_x_32gf"
+        regnet_y_400mf,regnet_y_800mf,regnet_y_1_6gf,regnet_y_3_2gf,regnet_y_8gf,regnet_y_16gf,regnet_y_32gf,regnet_x_400mf,regnet_x_800mf,regnet_x_1_6gf,regnet_x_3_2gf,regnet_x_8gf,regnet_x_16gf,regnet_x_32gf"
 fi
 if [ ${model_all} == "key" ]; then
     model_all="\
