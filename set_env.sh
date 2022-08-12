4916d2bc7782d324f51cab1ac10863b86b8d4b04
jianan-gu <jianan.gu@intel.com>
2022-08-11 19:53:02 -0700

    openvino env set

--- set_env.sh
+++ set_env.sh
@@ -5,6 +5,6 @@ conda install -y mkl mkl-include
 conda install -y jemalloc
 
 # installations
-pip install --no-deps Pillow munch tqdm six numpy typing-extensions geffnet torchvision
+pip install --no-deps Pillow munch tqdm six numpy typing-extensions geffnet torchvision torch-ort torch-ort-infer onnx onnxruntime-openvino onnxruntime-training psutil
 python pretrain_setup.py install
 rm -rf build dist pretrainedmodels.egg-info
