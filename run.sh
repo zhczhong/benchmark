370d440c9b74423b66c3a30fcb6ea11dad56e838
Zhang, Weizhuo <weizhuo.zhang@intel.com>
2022-09-08 17:11:11 +0000

    fix run.sh model_list bug

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 set -x
 
 mode_all=$1
-if [ ${model_all} == "all" ]; then
-    model_all="throughput,latency,multi_instance"
+if [ ${mode_all} == "all" ]; then
+    mode_all="throughput,latency,multi_instance"
 fi
 mode_list=($(echo "${mode_all}" |sed 's/,/ /g'))
 
@@ -11,14 +11,14 @@ do
     # bash run_auto_cpu.sh all float32 ${mode} ipex
     # bash run_auto_cpu.sh all float32 ${mode} torchdynamo_ipex
     #bash run_auto_cpu.sh all float32 ${mode} pt
-    #bash run_auto_cpu.sh all float32 ${mode} jit
+    bash run_auto_cpu.sh all float32 ${mode} jit
     #bash run_auto_cpu.sh all float32 ${mode} jit_ofi
     # bash run_auto_cpu.sh all bfloat16 ${mode} ipex
     # bash run_auto_cpu.sh all bfloat16 ${mode} torchdynamo_ipex
     #bash run_auto_cpu.sh all bfloat16 ${mode} pt
-    #bash run_auto_cpu.sh all bfloat16 ${mode} jit
+    bash run_auto_cpu.sh all bfloat16 ${mode} jit
     #bash run_auto_cpu.sh all int8_ipex ${mode} int8_ipex
 
     # OpenVino
-    bash run_auto_cpu.sh all FP32 ${mode} openvino
+    #bash run_auto_cpu.sh all FP32 ${mode} openvino
 done
