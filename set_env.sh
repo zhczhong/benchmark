f47ffcade4db5d3d3715e493e9174eb4fde9261a
Weizhuo Zhang <weizhuo.zhang@intel.com>
2023-02-05 22:34:00 -0800

    rm ort

--- set_env.sh
+++ set_env.sh
@@ -6,7 +6,7 @@ conda install -y jemalloc
 conda install -y numpy ninja pyyaml mkl mkl-include setuptools cmake cffi typing_extensions future six requests dataclasses psutil
 # installations
 pip install --no-deps Pillow munch tqdm six numpy typing-extensions geffnet torchvision onnx onnxruntime-openvino onnxruntime-training psutil
-pip install torch-ort
-pip install torch-ort-infer
+#pip install torch-ort
+#pip install torch-ort-infer
 python pretrain_setup.py install
 rm -rf build dist pretrainedmodels.egg-info
