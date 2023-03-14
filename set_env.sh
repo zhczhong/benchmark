2d73d1b98f9d7fc1bf11e4afcf550e6288b6b413
Weizhuo Zhang <weizhuo.zhang@intel.com>
2023-03-14 01:42:17 -0700

    pip install packages

--- set_env.sh
+++ set_env.sh
@@ -5,7 +5,7 @@ conda install -y mkl mkl-include
 conda install -y jemalloc
 conda install -y numpy ninja pyyaml mkl mkl-include setuptools cmake cffi typing_extensions future six requests dataclasses psutil
 # installations
-pip install --no-deps Pillow munch tqdm six numpy typing-extensions geffnet torchvision onnx onnxruntime-openvino onnxruntime-training psutil
+pip install --no-deps Pillow munch tqdm six numpy typing-extensions geffnet torchvision  psutil chardet cerberus flatbuffers h5py packaging protobuf google
 #pip install torch-ort
 #pip install torch-ort-infer
 pip install chardet cerberus flatbuffers h5py packaging protobuf google
