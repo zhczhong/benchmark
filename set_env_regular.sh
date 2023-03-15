6615599d211bff0ea27ffe65234baa768c86ea92
diwei sun <diwei.sun@intel.com>
2023-03-15 09:56:44 +0800

    nightly env config

--- set_env_regular.sh
+++ set_env_regular.sh
@@ -6,5 +6,6 @@ conda install -y jemalloc
 conda install -y numpy ninja pyyaml mkl mkl-include setuptools cmake cffi typing_extensions future six requests dataclasses psutil
 # installations
 pip install --no-deps Pillow munch tqdm six numpy typing-extensions geffnet onnx onnxruntime-openvino onnxruntime-training psutil
+pip install chardet cerberus flatbuffers h5py packaging protobuf google
 python pretrain_setup.py install
 rm -rf build dist pretrainedmodels.egg-info
