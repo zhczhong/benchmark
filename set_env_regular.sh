4b4d6b91c77f91b5cdac7faeb070cbf717f663ee
DiweiSun <105627594+DiweiSun@users.noreply.github.com>
2023-06-01 11:07:37 +0800

    Update set_env_regular.sh

--- set_env_regular.sh
+++ set_env_regular.sh
@@ -6,6 +6,6 @@ conda install -y jemalloc
 conda install -y numpy ninja pyyaml mkl mkl-include setuptools cmake cffi typing_extensions future six requests dataclasses psutil
 # installations
 pip install --no-deps Pillow munch tqdm six numpy typing-extensions geffnet onnx onnxruntime-openvino onnxruntime-training psutil
-pip install chardet cerberus flatbuffers h5py packaging protobuf google
+pip install chardet cerberus flatbuffers h5py packaging protobuf google chardet
 python pretrain_setup.py install
 rm -rf build dist pretrainedmodels.egg-info
