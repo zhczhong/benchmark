f770d89b9c8aa7f25b3b06108a21f538857b63b3
Ciyong Chen <ciyong.chen@intel.com>
2023-07-28 15:23:43 +0800

    restore 4 vit models and remove duplicate efficientnet_b0

--- run_oob_model.py
+++ run_oob_model.py
@@ -44,7 +44,7 @@ torch_vision_classification_model_list = [
     'shufflenet_v2_x1_0', 'shufflenet_v2_x1_5', 'shufflenet_v2_x2_0',
     'squeezenet1_0', 'squeezenet1_1', 'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn',
     'vgg16', 'vgg16_bn', 'vgg19', 'vgg19_bn', 'wide_resnet101_2',
-    'wide_resnet50_2'
+    'wide_resnet50_2', "vit_b_16", "vit_b_32", "vit_l_16", "vit_l_32"
 ]  # image_size 224, skip: "efficientnet_v2_s", "efficientnet_v2_m","efficientnet_v2_l", "'regnet_y_128gf'", 'vit_b_16','vit_b_32', 'vit_l_16', 'vit_l_32', 'swin_b', 'swin_s','swin_t', 'swin_v2_b', 'swin_v2_s', 'swin_v2_t'
 
 torch_vision_segmentation_model_list = [
@@ -57,8 +57,7 @@ VIT_model_list = ["DeepViT"]
 
 timm_vision_model_list = [
     "fbnetc_100", "spnasnet_100", "vit_small_patch16_224",
-    "vit_giant_patch14_224", "regnety_120", "resnest14d", "efficientnet_b0"
-]
+    "vit_giant_patch14_224", "regnety_120", "resnest14d"]
 
 group_conv_model_list = ['regnet_x_16gf', 'regnet_x_1_6gf', 'regnet_x_32gf',
     'regnet_x_3_2gf', 'regnet_x_400mf', 'regnet_x_800mf', 'regnet_x_8gf',
